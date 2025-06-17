32627b576cb6c85b47925012248645dc7f6f7310
Vladyslav Tomchuk <tomchukvlad92@gmail.com>
2025-06-17 17:28:47 +0200

    Fix: add __dirname for ESM in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,15 @@
+// next.config.mjs
 import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
-    config.resolve.alias["@"] = path.resolve("./src");
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
     return config;
   },
 };
