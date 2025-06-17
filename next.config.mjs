2eef4467ee87b99ad74e5e2ab5e54ab53cda1a06
Vladyslav Tomchuk <tomchukvlad92@gmail.com>
2025-06-17 17:58:52 +0200

    Fix path alias with process.cwd()

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,9 @@
-// next.config.mjs
 import path from "path";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
-    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    config.resolve.alias["@"] = path.resolve(process.cwd(), "src");
     return config;
   },
 };
